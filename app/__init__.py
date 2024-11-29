ee9fb6cd75ef01b165f16cf26dbde5df9830c1da
Pedro Luis Pérez Collado <81271305+plpcollado@users.noreply.github.com>
2024-11-28 18:18:35 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from model import CityModel
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Create an instance of the CityModel
 city_model = CityModel(num_cars=5, width=24, height=24)
@@ -9,10 +11,21 @@ city_model = CityModel(num_cars=5, width=24, height=24)
 # Define a route to get car positions
 @app.route('/car_positions', methods=['GET'])
 def get_car_positions():
+    # Initialize a new model instance instead of resetting
+    global city_model
+    city_model = CityModel(num_cars=5, width=24, height=24)
     positions_over_time = []
-    for step in range(30):
+    
+    # Get initial positions when cars are in parking lots
+    positions = city_model.get_car_positions()
+    positions_step = {'step': 0, 'cars': positions}
+    positions_over_time.append(positions_step)
+    
+    # Run model for 30 steps and collect positions at each step
+    for step in range(1, 31):
         city_model.step()
         positions = city_model.get_car_positions()
         positions_step = {'step': step, 'cars': positions}
         positions_over_time.append(positions_step)
+        
     return jsonify(positions_over_time)
